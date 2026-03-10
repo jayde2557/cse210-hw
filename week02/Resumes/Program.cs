d65fc1607953168652d94ab224d30181fe7b0951
jayde2557 <jbaidoo1@byupathway.edu>
2026-03-10 16:08:57 +0000

    abstraction&classes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Jayden Baidoo";
+        myResume._jobs.Add(job1);
+        myResume.Display();
+
+
+    }   
 }
